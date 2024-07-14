16c3d97eedda89fba2e88b83fa4adf8889649b16
AymanUzayr <68193240+AymanUzayr@users.noreply.github.com>
2024-07-15 01:42:29 +0530

    Update TCPclient.c

--- TCPclient.c
+++ TCPclient.c
@@ -5,11 +5,10 @@
 #include <unistd.h>
 #include <string.h>
 
-#define PORT 5600
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in server_addr = { .sin_family = AF_INET, .sin_port = htons(PORT) };
+    struct sockaddr_in server_addr = { .sin_family = AF_INET, .sin_port = htons(5600) };
 
     if (sockfd < 0 || connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Error");
